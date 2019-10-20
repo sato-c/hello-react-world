b1776a2049c432071ae25b9be52a305070842ef5
sato_c <motosumi64@gmail.com>
2019-10-20 20:24:00 +0900

    基本のファイル

---
+++ App.tsx
@@ -0,0 +1,20 @@
+interface WelcomeProps {
+    name: string;
+  }
+  
+  function Welcome(props: WelcomeProps) {
+    return <h1>Hello, {props.name}</h1>;
+  }
+  
+  const App: React.FC = () => {
+    return (
+      <div className="App">
+        <Welcome name="Sara" />
+        <Welcome name="Cahal" />
+        <Welcome name="Edite" />
+        <Welcome name="Everyone" />
+      </div>
+    );
+  }
+  
+  export default App;
